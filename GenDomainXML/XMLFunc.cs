f568b15e03bf86f373d63eb70c403836681f12a4
DESKTOP-H9H8I8F\admin <mail22sms@mail.ru>
2024-08-28 19:17:18 +0300

    *

--- GenDomainXML/XMLFunc.cs
+++ GenDomainXML/XMLFunc.cs
@@ -16,10 +16,24 @@ namespace GenDomainXML
             XmlDocument xmlDoc = new XmlDocument();
 
             XmlElement root = xmlDoc.CreateElement("Alpha.Net.Agent"); // создаем элемент Alpha.Net.Agent
-            XmlAttribute _name = xmlDoc.CreateAttribute("name"); // создаем атрибут namr
+            
+            XmlAttribute _name = xmlDoc.CreateAttribute("name"); // создаем атрибут name
+            XmlAttribute _NetEnterPort = xmlDoc.CreateAttribute("NetEnterPort");
+            XmlAttribute _ParentAgentPort = xmlDoc.CreateAttribute("ParentAgentPort");
+
             XmlText _nameText = xmlDoc.CreateTextNode("local"); // просто создаем значение local
+            XmlText _NetEnterPortText = xmlDoc.CreateTextNode("1010");
+            XmlText _ParentAgentPortText= xmlDoc.CreateTextNode("1012");
+
             _name.AppendChild(_nameText); // применяем значение local к атрибуту name
+            _NetEnterPort.AppendChild(_NetEnterPortText);
+            _ParentAgentPort.AppendChild(_ParentAgentPortText);
+
+
             root.Attributes.Append(_name); // добавляем созданный атрибут в рутовый элемент 
+            root.Attributes.Append(_NetEnterPort);
+            root.Attributes.Append(_ParentAgentPort);
+
             xmlDoc.AppendChild(root); // добавляем в основной xml
             xmlDoc.Save("net.xml");
 
