1438dddfc82481917716a0028ba057f7c5d0b389
DESKTOP-H9H8I8F\admin <mail22sms@mail.ru>
2024-08-28 19:09:40 +0300

    xml

--- GenDomainXML/XMLFunc.cs
+++ GenDomainXML/XMLFunc.cs
@@ -10,18 +10,20 @@ namespace GenDomainXML
     internal class XMLFunc
     {
         public static void genLocalNetXML() //XmlNode _dataIn
-        { 
+        {
+            // https://metanit.com/sharp/tutorial/16.3.php
+
             XmlDocument xmlDoc = new XmlDocument();
 
-            XmlElement root = xmlDoc.CreateElement("Alpha.Net.Agent");
-            xmlDoc.AppendChild(root);
-           
-            XmlElement _name = xmlDoc.CreateElement("Name");
-            _name.InnerText = "local";
-            root.AppendChild(_name);
-
+            XmlElement root = xmlDoc.CreateElement("Alpha.Net.Agent"); // создаем элемент Alpha.Net.Agent
+            XmlAttribute _name = xmlDoc.CreateAttribute("name"); // создаем атрибут namr
+            XmlText _nameText = xmlDoc.CreateTextNode("local"); // просто создаем значение local
+            _name.AppendChild(_nameText); // применяем значение local к атрибуту name
+            root.Attributes.Append(_name); // добавляем созданный атрибут в рутовый элемент 
+            xmlDoc.AppendChild(root); // добавляем в основной xml
             xmlDoc.Save("net.xml");
-        
+
+
         }
     }
 }
